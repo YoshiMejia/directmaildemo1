44921d148efc3689c6041b7672ab9d8125122a75
Yoshi <yoshimejia00@gmail.com>
2023-05-18 21:01:17 -0700

    conditionally rendering download button

--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import DownloadButton from '@/components/DownloadButton';
 
 const Form = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [selectedTemplate, setSelectedTemplate] = useState('none');
   const [successfulConversion, setSuccessfulConversion] = useState(false);
+  const [fileList, setFileList] = useState([]);
 
   const handleFileUpload = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -12,14 +14,11 @@ const Form = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
-    console.log('formData', formData);
     formData.append('csv', selectedFile);
     formData.append('template', selectedTemplate);
+    console.log('formData', formData);
     try {
       const response = await fetch('http://localhost:8000/convert', {
-        // const response = await fetch(
-        //   'https://yoshimejia.github.io/directmaildemo/convert',
-        //   {
         method: 'POST',
         body: formData,
       });
@@ -30,37 +29,41 @@ const Form = () => {
       }
       const convertedData = await response.json();
       console.log(convertedData);
-      const fileList = await response.json();
-
-      fileList.forEach((filePath) => {
-        <a href={filePath} download>
-          Download
-        </a>;
-      });
+      setSuccessfulConversion(true);
+      setFileList(convertedData);
+      // check back if setFileList/fileList state values are needed now that download button is working, also check if ConvertedFiles component is needed
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div id="csv-form">
-      <form onSubmit={handleSubmit}>
-        <input type="file" accept=".csv" onChange={handleFileUpload} />
-        <label htmlFor="template1">template1</label>
-        <input
-          type="checkbox"
-          value="template1"
-          onClick={(event) => setSelectedTemplate(event.target.value)}
-        />
-        <label htmlFor="template2">template2</label>
-        <input
-          type="checkbox"
-          value="template2"
-          onClick={(event) => setSelectedTemplate(event.target.value)}
-        />
-        <button type="submit">Convert</button>
-      </form>
-    </div>
+    <>
+      <div id="csv-form">
+        <form onSubmit={handleSubmit}>
+          <input type="file" accept=".csv" onChange={handleFileUpload} />
+          <label htmlFor="template1">template1</label>
+          <input
+            type="checkbox"
+            value="template1"
+            onClick={(event) => setSelectedTemplate(event.target.value)}
+          />
+          <label htmlFor="template2">template2</label>
+          <input
+            type="checkbox"
+            value="template2"
+            onClick={(event) => setSelectedTemplate(event.target.value)}
+          />
+          <button type="submit">Convert</button>
+        </form>
+      </div>
+
+      {successfulConversion && (
+        <div>
+          <DownloadButton />
+        </div>
+      )}
+    </>
   );
 };
 
