5eb40db590c556e4fd952618e91295bd1778c1b1
Yoshi <yoshimejia00@gmail.com>
2023-05-25 16:36:46 -0700

    added comment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,18 @@
 import { SessionProvider } from 'next-auth/react';
 import '../app/globals.css';
+import RootLayout from '@/app/layout';
+import { NavHead } from '@/app/components/NavHead';
+import Footer from '@/app/components/Footer';
 
 function App({ Component, pageProps, session }) {
   return (
+    // getting hydration error when using RootLayout. Need to figure out why.
     <SessionProvider session={session}>
+      {/* <RootLayout> */}
+      <NavHead />
       <Component {...pageProps} />
+      <Footer />
+      {/* </RootLayout> */}
     </SessionProvider>
   );
 }
