4fa6910a74c10bf983e945f594fa2291e98e7fba
Yoshi <yoshimejia00@gmail.com>
2023-05-25 09:43:24 -0700

    updated homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import Link from 'next/link';
+import Image from 'next/image';
+import LoginModal from '@/containers/LoginModal';
+import { NavHead } from '@/app/components/NavHead';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/Form">CSV form</Link>
+      <NavHead />
+      {/* <div className="relative flex place-items-center "> */}
+      <LoginModal />
+      {/* </div> */}
     </main>
   );
 }
