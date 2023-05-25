bf8b8391840543360fe71b79a0b98c47f635a829
Yoshi <yoshimejia00@gmail.com>
2023-05-25 16:34:36 -0700

    rendering login modal only

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
 import Image from 'next/image';
-import LoginModal from '@/containers/LoginModal';
+import LoginModal from '@/pages/components/LoginModal';
 import { NavHead } from '@/app/components/NavHead';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <NavHead />
-      {/* <div className="relative flex place-items-center "> */}
+    <main className="flex flex-col items-center">
       <LoginModal />
-      {/* </div> */}
     </main>
   );
 }
