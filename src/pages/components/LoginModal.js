53b6219e443ac5ca01b1e95c52c4b3b2fb0e1791
Yoshi <yoshimejia00@gmail.com>
2023-06-10 15:22:33 -0400

    login modal init commit

---
+++ src/pages/components/LoginModal.js
@@ -0,0 +1,13 @@
+'use client';
+import Link from 'next/link';
+import LoginPage from '@/app/login/page';
+
+const LoginModal = () => {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+};
+
+export default LoginModal;
