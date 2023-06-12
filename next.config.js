b7343062ab9b48d4712abb646ef6720faa622877
Yoshi <yoshimejia00@gmail.com>
2023-06-12 16:24:08 -0400

    nextauth and image config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  secret: process.env.NEXTAUTH_SECRET,
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
