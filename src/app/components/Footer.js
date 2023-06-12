c3ccfbada24f1863c6fd96525d6dd7bde3f2e5a8
Yoshi <yoshimejia00@gmail.com>
2023-06-12 19:02:50 -0400

    removing comment

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -22,13 +22,6 @@ const Footer = () => {
                 alt="user image"
                 className="inline mr-4"
               ></Image>
-              {/* <img
-                src={data?.user?.image}
-                height="25"
-                width="25"
-                alt="user image"
-                className="inline mr-4"
-              /> */}
               You are currently logged in.
             </span>
             <span className="float-right" onClick={() => signOut()}>
