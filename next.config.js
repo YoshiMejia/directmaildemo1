26cd1be8cbec900990b148dbb0f4c730122f1ba1
Yoshi <yoshimejia00@gmail.com>
2023-06-12 16:50:32 -0400

    adjusting secret

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
 };
-
 module.exports = nextConfig;
