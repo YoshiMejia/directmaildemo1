e4213e67b7c7370b4cd68ba99873be8201045f07
Yoshi <yoshimejia00@gmail.com>
2023-06-13 02:49:19 -0400

    removing comment

--- src/pages/components/FileList.js
+++ src/pages/components/FileList.js
@@ -1,4 +1,8 @@
 const FileList = ({ fileList }) => {
+  if (!fileList) {
+    return <div>No files to show.</div>;
+  }
+
   const openHtmlPreview = (filename) => {
     const htmlContent = fileList[filename];
     const newWindow = window.open('', '_blank');
@@ -29,55 +33,3 @@ const FileList = ({ fileList }) => {
 };
 
 export default FileList;
-
-// import React, { useState } from 'react';
-
-// const FileList = () => {
-//   const [fileData, setFileData] = useState({});
-
-//   const fetchFileData = () => {
-//     fetch('http://localhost:8000/converted/success')
-//       .then((response) => response.json())
-//       .then((data) =>
-//         setFileData(
-//           data.files.reduce((acc, file) => {
-//             const content = file.content;
-//             return {
-//               ...acc,
-//               [file.filename]: content,
-//             };
-//           }, {})
-//         )
-//       )
-//       .catch((error) => console.error('Error fetching file data:', error));
-//   };
-
-//   const openHtmlPreview = (filename) => {
-//     const htmlContent = fileData[filename];
-//     const newWindow = window.open('', '_blank');
-//     newWindow.document.write(htmlContent);
-//     newWindow.document.close();
-//   };
-
-//   return (
-//     <div>
-//       <h1>Converted Files</h1>
-//       <button onClick={fetchFileData}>Preview conversions</button>
-//       {Object.keys(fileData).length > 0 ? (
-//         <ul>
-//           {Object.entries(fileData).map(([filename, content]) => (
-//             <li key={filename}>
-//               <a href="#" onClick={() => openHtmlPreview(filename)}>
-//                 {filename}
-//               </a>
-//             </li>
-//           ))}
-//         </ul>
-//       ) : (
-//         console.log('no files to display')
-//       )}
-//     </div>
-//   );
-// };
-
-// export default FileList;
