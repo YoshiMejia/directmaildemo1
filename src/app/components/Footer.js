5beab79d14382920d4c1f30a85242050a78d4da9
Yoshi <yoshimejia00@gmail.com>
2023-06-14 19:27:23 -0400

    added CSS

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -10,34 +10,46 @@ const Footer = () => {
   console.log('log data, inside Footer:', data);
 
   return (
-    <footer className="relative left-44 max-w-5xl border-t-4 mt-20 mb-16 pt-2 font-mono">
-      <div className="flex-row ">
-        {data?.user ? (
-          <>
-            <span>
-              <Image
-                src={data?.user?.image}
-                height="25"
-                width="25"
-                alt="user image"
-                className="inline mr-4"
-              ></Image>
-              You are currently logged in.
-            </span>
-            <span
-              className="float-right cursor-pointer"
-              onClick={() => signOut()}
-            >
-              Logout
-            </span>
-          </>
-        ) : (
-          <span className="float-right">
-            <Link className="nav-link" href="/login">
-              Login
-            </Link>
-          </span>
-        )}
+    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023 WAConverter . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            {data?.user ? (
+              <>
+                <span>
+                  <Image
+                    src={data?.user?.image}
+                    height="25"
+                    width="25"
+                    alt="user image"
+                    className="inline mr-4"
+                  ></Image>
+                  You are currently logged in.
+                </span>
+                <span
+                  className="float-right cursor-pointer"
+                  onClick={() => signOut()}
+                >
+                  Logout
+                </span>
+              </>
+            ) : (
+              <li>
+                <span className="float-right">
+                  <Link
+                    href="/login"
+                    className=" nav-link mr-4 hover:underline md:mr-6 "
+                  >
+                    Login
+                  </Link>
+                </span>
+              </li>
+            )}
+          </li>
+        </ul>
       </div>
     </footer>
   );
