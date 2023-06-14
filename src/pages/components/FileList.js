cea497a5b9e856c7e3a177a5ccd230bc11801de3
Yoshi <yoshimejia00@gmail.com>
2023-06-14 19:16:51 -0400

    added CSS

--- src/pages/components/FileList.js
+++ src/pages/components/FileList.js
@@ -1,6 +1,6 @@
 const FileList = ({ fileList }) => {
   if (!fileList) {
-    return <div>No files to show.</div>;
+    return <h1 className="font-mono">There are currently no files to show.</h1>;
   }
 
   const openHtmlPreview = (filename) => {
@@ -13,20 +13,26 @@ const FileList = ({ fileList }) => {
   return (
     <div>
       {Object.keys(fileList).length > 0 ? (
-        <ul className="w-96">
-          {Object.entries(fileList).map(([filename, content]) => (
-            <li
-              key={filename}
-              className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50"
-            >
-              <a href="#" onClick={() => openHtmlPreview(filename)}>
-                {filename}
-              </a>
-            </li>
-          ))}
-        </ul>
+        <div className="bg-white z-20 rounded-lg p-2 space-x-4 relative top-28 h-fit -left-12 w-80 border-2 overflow-hidden">
+          <ul className="w-96">
+            {Object.entries(fileList).map(([filename, content]) => (
+              <li
+                key={filename}
+                className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50"
+              >
+                <a href="#" onClick={() => openHtmlPreview(filename)}>
+                  {filename}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (
-        <div>No files to show.</div>
+        <div className="bg-white z-20 rounded-lg relative border-2 h-fit top-60 p-6 -left-24">
+          <h1 className="font-mono text-xl">
+            There are currently no files to show.
+          </h1>
+        </div>
       )}
     </div>
   );
