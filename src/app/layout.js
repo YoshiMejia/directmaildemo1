89134993b20c626faf0900b3fe6445b2da8f50e5
Yoshi <yoshimejia00@gmail.com>
2023-10-27 16:26:46 -0700

    removed font-mono styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import { SessionProvider } from 'next-auth/react';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="font-mono">
+      <body>
         <SessionProvider>
           <NavHead />
           {children}
