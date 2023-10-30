1aedd7099d01da30b53a98197c211efab5aebccf
Yoshi <yoshimejia00@gmail.com>
2023-10-30 14:11:49 -0700

    adding in max width wrapper

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,20 @@ import './globals.css';
 import { NavHead } from './components/NavHead';
 import Footer from './components/Footer';
 import { SessionProvider } from 'next-auth/react';
+import { cn } from '../lib/utils';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={cn(
+          'min-h-screen font-sans antialiased grainy',
+          inter.className
+        )}
+      >
         <SessionProvider>
           <NavHead />
           {children}
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
 import Image from 'next/image';
-import LoginModal from '@/pages/components/LoginModal';
-import { NavHead } from '@/app/components/NavHead';
+import LoginModal from '../pages/components/LoginModal';
+import { NavHead } from '../app/components/NavHead';
 import MaxWidthWrapper from '../pages/components/MaxWidthWrapper';
 
 export default function Home() {
   return (
-    <MaxWidthWrapper>
-      <main className="flex flex-col items-center">
-        <LoginModal />
-      </main>
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">
+          CSV Converter is under construction
+        </p>
+      </div>
+
+      <LoginModal />
     </MaxWidthWrapper>
   );
 }
