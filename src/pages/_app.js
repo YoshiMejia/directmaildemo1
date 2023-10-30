687038f0144156957d000ae342532c943385f1a3
Yoshi <yoshimejia00@gmail.com>
2023-10-30 14:40:11 -0700

    adjusting imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,8 @@
 import { SessionProvider } from 'next-auth/react';
 import '../app/globals.css';
-import RootLayout from '@/app/layout';
-import { NavHead } from '@/app/components/NavHead';
-import Footer from '@/app/components/Footer';
+import RootLayout from '../app/layout';
+import { NavHead } from '../app/components/NavHead';
+import Footer from '../app/components/Footer';
 
 function App({ Component, pageProps, session }) {
   return (
