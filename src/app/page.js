e0ff485595f629944c737986d5569ad70d4df25a
Yoshi <yoshimejia00@gmail.com>
2023-10-30 13:26:31 -0700

    adding in wrapper for max width

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import LoginModal from '@/pages/components/LoginModal';
 import { NavHead } from '@/app/components/NavHead';
+import MaxWidthWrapper from '../pages/components/MaxWidthWrapper';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
-      <LoginModal />
-    </main>
+    <MaxWidthWrapper>
+      <main className="flex flex-col items-center">
+        <LoginModal />
+      </main>
+    </MaxWidthWrapper>
   );
 }
